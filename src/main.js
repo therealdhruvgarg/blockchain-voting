0ed6cd87dbfaedc06642488c074eaaa5810a4197
therealdhruvgarg <dhruv.g4445@gmail.com>
2024-11-01 20:04:55 +0530

    Refactor blockchain structure: Introduce Transactions class and update Block and BlockChain classes for improved transaction handling and mining logic

--- src/main.js
+++ src/main.js
@@ -1,58 +1,76 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require("crypto-js/sha256");
 
-class Block{
-    constructor(index, timestamp, data, previousHash = ''){
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-    }
-    calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)+this.nonce).toString();
-    }
-    mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-        console.log("Block mined: " + this.hash);
-    }
+class Transactions {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
 }
 
-class BlockChain{
-    constructor(){
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 5;
+class Block {
+  constructor(timestamp, transactions, previousHash = "") {
+    this.timestamp = timestamp;
+    this.transactions = transactions;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
     }
-    createGenesisBlock(){
-        return new Block(0, "01/01/2024", "Genesis block", "0");
-    }
-    getLatestBlock(){
-        return this.chain[this.chain.length - 1];
-    }
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
-    }
-    isChainValid(){
-        for(let i=1; i<this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i-1];
-            if(currentBlock.hash !== currentBlock.calculateHash()){
-                return false;
-            }
-            if(currentBlock.previousHash !== previousBlock.hash){
-                return false;
-            }
-        }
-        return true;
-    }
+    console.log("Block mined: " + this.hash);
+  }
 }
 
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
+  }
+  createGenesisBlock() {
+    return new Block("01/01/2024", "Genesis block", "0");
+  }
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  minedPendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions);
+    block.mineBlock(this.difficulty);
+    console.log("Block successfully mined!");
+    this.chain.push(block);
+    this.pendingTransactions = [
+      new Transactions(null, miningRewardAddress, this.miningReward),
+    ];
+  }
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
 
 let dhruvCoin = new BlockChain();
-
-
